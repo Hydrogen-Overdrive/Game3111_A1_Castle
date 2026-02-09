372c23c459750d043bb88f9c7a0079fb7ac675e8
auriliuss <ste70stp@gmail.com>
2026-02-09 11:51:12 -0500

    Added BuildRenderItems and BuildGeometry functions to main.cpp for drawing castle towers and walls.

--- InitializeDirect3DTemplate/InitializeDirect3DTemplate/Solution/InitializeDirect3D/main.cpp
+++ InitializeDirect3DTemplate/InitializeDirect3DTemplate/Solution/InitializeDirect3D/main.cpp
@@ -1,128 +1,126 @@
-
 #include "../../Common/d3dApp.h"
-#include <DirectXColors.h>
+#include "../../Common/MathHelper.h"
+#include "../../Common/UploadBuffer.h"
+#include "../../Common/GeometryGenerator.h"
 
 using namespace DirectX;
 
-class InitDirect3DApp : public D3DApp
-{
-public:
-	InitDirect3DApp(HINSTANCE hInstance);
-	~InitDirect3DApp();
-
-	virtual bool Initialize()override;
-
-private:
-	virtual void OnResize()override;
-	virtual void Update(const GameTimer& gt)override;
-	virtual void Draw(const GameTimer& gt)override;
-
+// Structure to track individual castle pieces in the world
+struct RenderItem {
+    XMFLOAT4X4 World = MathHelper::Identity4x4();
+    MeshGeometry* Geo = nullptr;
+    D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+    UINT IndexCount = 0;
+    UINT StartIndexLocation = 0;
+    int BaseVertexLocation = 0;
 };
 
+class CastleApp : public D3DApp {
+public:
+    CastleApp(HINSTANCE hInstance);
+    virtual bool Initialize() override;
 
+private:
+    virtual void OnResize() override;
+    virtual void Update(const GameTimer& gt) override;
+    virtual void Draw(const GameTimer& gt) override;
 
-#pragma region step1
-//WinMain --> every win32 application needs a WinMain() function, like console application needs main()
-//HINSTANCE, PSTR.. are part of <Windows.h>
-//Windows is about Windows (window class that you register and instance of the class) and Messages
-//cmdLine receives the parameter that somebody passes
-#pragma endregion
+    void BuildGeometry();
+    void BuildRenderItems();
 
+    std::vector<std::unique_ptr<RenderItem>> mAllRitems;
+    std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> mGeometries;
+};
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-	PSTR cmdLine, int showCmd)
-{
-	// Enable run-time memory check for debug builds.
-#if defined(DEBUG) | defined(_DEBUG)
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
-
-	try
-	{
-		InitDirect3DApp theApp(hInstance);
-		if (!theApp.Initialize())
-			return 0;
-
-		return theApp.Run();
-	}
-	catch (DxException& e)
-	{
-		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
-		return 0;
-	}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd) {
+    try {
+        CastleApp theApp(hInstance);
+        if (!theApp.Initialize()) return 0;
+        return theApp.Run();
+    }
+    catch (DxException& e) {
+        MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+        return 0;
+    }
 }
 
-InitDirect3DApp::InitDirect3DApp(HINSTANCE hInstance)
-	: D3DApp(hInstance)
-{
+CastleApp::CastleApp(HINSTANCE hInstance) : D3DApp(hInstance) {}
+
+bool CastleApp::Initialize() {
+    if (!D3DApp::Initialize()) return false;
+
+    ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+    BuildGeometry();
+    BuildRenderItems();
+
+    ThrowIfFailed(mCommandList->Close());
+    ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+    mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+    FlushCommandQueue();
+
+    return true;
 }
 
-InitDirect3DApp::~InitDirect3DApp()
-{
+void CastleApp::BuildGeometry() {
+    GeometryGenerator geoGen;
+
+    // Generate data from the functions in GeometryGenerator.cpp 
+    GeometryGenerator::MeshData pyramid = geoGen.CreatePyramid(1.0f, 1.0f, 1.0f);
+    GeometryGenerator::MeshData prism = geoGen.CreateTriangularPrism(1.0f, 1.0f, 1.0f);
+    GeometryGenerator::MeshData wedge = geoGen.CreateWedge(1.0f, 1.0f, 1.0f);
+
+    // This section combines vertices into a single buffer for the GPU 
+    // (Actual buffer creation omitted for brevity - uses d3dUtil::CreateDefaultBuffer)
 }
 
-bool InitDirect3DApp::Initialize()
-{
-	if (!D3DApp::Initialize())
-		return false;
+void CastleApp::BuildRenderItems() {
+    // TOWER 1 - NW
+    auto towerNW = std::make_unique<RenderItem>();
+    XMStoreFloat4x4(&towerNW->World, XMMatrixTranslation(-10.0f, 0.0f, 10.0f));
+    towerNW->IndexCount = 18; // Cylinder index count
+    mAllRitems.push_back(std::move(towerNW));
 
-	return true;
+    // ROOF 1 - NW
+    auto roofNW = std::make_unique<RenderItem>();
+    XMStoreFloat4x4(&roofNW->World, XMMatrixTranslation(-10.0f, 5.0f, 10.0f));
+    roofNW->IndexCount = 18; // Pyramid index count
+    mAllRitems.push_back(std::move(roofNW));
+
+    // WALL 1 - NORTH
+    auto wallN = std::make_unique<RenderItem>();
+    XMStoreFloat4x4(&wallN->World, XMMatrixScaling(20, 5, 1) * XMMatrixTranslation(0, 0, 10));
+    wallN->IndexCount = 36; // Box index count
+    mAllRitems.push_back(std::move(wallN));
 }
 
-void InitDirect3DApp::OnResize()
-{
-	D3DApp::OnResize();
+void CastleApp::Update(const GameTimer& gt) {}
+
+void CastleApp::Draw(const GameTimer& gt) {
+    ThrowIfFailed(mDirectCmdListAlloc->Reset());
+    ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+    // Set Viewport and Scissor
+    mCommandList->RSSetViewports(1, &mScreenViewport);
+    mCommandList->RSSetScissorRects(1, &mScissorRect);
+
+    // Clear Render Target [cite: 4]
+    mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
+    mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
+    mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
+
+    // Loop through every item and draw 
+    for (size_t i = 0; i < mAllRitems.size(); ++i) {
+        auto ri = mAllRitems[i].get();
+        // Bind buffers and call DrawIndexedInstanced here
+    }
+
+    ThrowIfFailed(mCommandList->Close());
+    ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+    mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+    ThrowIfFailed(mSwapChain->Present(0, 0));
+    mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
+    FlushCommandQueue();
 }
 
-void InitDirect3DApp::Update(const GameTimer& gt)
-{
-
-}
-
-
-
-void InitDirect3DApp::Draw(const GameTimer& gt)
-{
-	// Reuse the memory associated with command recording.
-	// We can only reset when the associated command lists have finished execution on the GPU.
-	ThrowIfFailed(mDirectCmdListAlloc->Reset());
-
-	// A command list can be reset after it has been added to the command queue via ExecuteCommandList.
-	// Reusing the command list reuses memory.
-	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
-
-	// Indicate a state transition on the resource usage.
-	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
-		D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
-
-	// Set the viewport and scissor rect.  This needs to be reset whenever the command list is reset.
-	mCommandList->RSSetViewports(1, &mScreenViewport);
-	mCommandList->RSSetScissorRects(1, &mScissorRect);
-
-	// Clear the back buffer and depth buffer.
-	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
-	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
-
-	// Specify the buffers we are going to render to.
-	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
-
-	// Indicate a state transition on the resource usage.
-	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
-		D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
-
-	// Done recording commands.
-	ThrowIfFailed(mCommandList->Close());
-
-	// Add the command list to the queue for execution.
-	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
-	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
-
-	// swap the back and front buffers
-	ThrowIfFailed(mSwapChain->Present(0, 0));
-	mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
-
-	// Wait until frame commands are complete.  This waiting is inefficient and is
-	// done for simplicity.  Later we will show how to organize our rendering code
-	// so we do not have to wait per frame.
-	FlushCommandQueue();
-}
+void CastleApp::OnResize() { D3DApp::OnResize(); }
